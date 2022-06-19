7c092d1f4576be71379672e04c33b09a4fc7a670
Pedro Soares <pedrosoaresty@gmail.com>
2022-06-19 00:21:24 -0300

    teste

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectiviesComponent } from './componentst/directivies/directivies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentDataComponent,
+    DirectiviesComponent
   ],
   imports: [
     BrowserModule
